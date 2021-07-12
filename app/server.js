fbfff63bf24a3a677e8455fa8b3b91e7ce34ac11
José Maria Labarta <59398281+jmlcas@users.noreply.github.com>
2021-07-12 23:50:47 +0200

    Update server.js

--- app/server.js
+++ app/server.js
@@ -21,7 +21,7 @@ app.get('/profile-picture', function (req, res) {
 });
 
 // use when starting application locally
-let mongoUrlLocal = "mongodb://admin:p4ssw0rd@localhost:27017";
+//let mongoUrlLocal = "mongodb://admin:p4ssw0rd@localhost:27017";
 
 // use when starting application as docker container
 let mongoUrlDocker = "mongodb://admin:p4ssw0rd@mongodb";
