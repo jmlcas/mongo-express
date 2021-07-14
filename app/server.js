449cf6db74e980d5e63f83e514ffaf0a68f00be4
José Maria Labarta <59398281+jmlcas@users.noreply.github.com>
2021-07-14 11:41:58 +0200

    Update server.js

--- app/server.js
+++ app/server.js
@@ -21,10 +21,10 @@ app.get('/profile-picture', function (req, res) {
 });
 
 // use when starting application locally
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+let mongoUrlLocal = "mongodb://admin:p4ssw0rd@localhost:27017";
 
 // use when starting application as docker container
-let mongoUrlDocker = "mongodb://admin:password@mongodb";
+let mongoUrlDocker = "mongodb://admin:p4ssw0rd@mongodb";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
